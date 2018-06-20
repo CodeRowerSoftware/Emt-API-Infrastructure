734e7a7b9a33865e4f0a4af910793ba10eb3e9c1
Ishaan Puniani <ishaan.puniani@gmail.com>
2018-06-20 22:30:17 +0200

    v0.0.3

---
+++ src/lib/AppConstants.ts
@@ -0,0 +1,8 @@
+namespace AppConstants {
+  export const Global = {
+    ADD_MESSAGE: 'Global.addMessageToQueue',
+    REMOVE_MESSAGE: 'Global.removeMessageFromQueue'
+  };
+}
+
+export default AppConstants;
---
+++ src/lib/AppEnums.ts
@@ -0,0 +1,8 @@
+namespace AppEnums {
+  export enum MessageType {
+    Toast,
+    Dialog
+  }
+}
+
+export default AppEnums;
